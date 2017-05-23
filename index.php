2b2b6c4a7c85b983c795fab068bdfb0235d26396
Richard Oh <rso136@gmail.com>
2017-05-22 21:20:55 -0400

    Added session contingency for index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 	include 'header.php'
+
+	if (isset($_SESSION['id'])) {
+        
+        header("Location: home.php");
+    }
 ?>
 <div class="jumbotron">
 	<h1 id="jumboHeading">ESSENTIALS</h1>
